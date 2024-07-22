43236548080cec93bb63ece017db0f37a29ba871
Haile Adane Assefa <109754116+hulezak@users.noreply.github.com>
2024-07-22 03:59:20 -0300

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+// const x=2
 const app = express();
 const port = 5000;
 
